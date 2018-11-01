ccb2e3f7d03a8f5b044de106255bc0051a65a99a
Connor Gilroy <ccgilroy@gmail.com>
2018-10-31 17:23:45 -0700

    reorganize - new additional readings section, pdf version, clean up reading list chapter

--- _build.sh
+++ _build.sh
@@ -2,3 +2,4 @@
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::word_document2')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
